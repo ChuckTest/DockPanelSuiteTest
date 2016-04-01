1f0ebd0fb64df419784c164edac97562e0a605d9
ChuckLu <244657538@qq.com>
2016-04-01 15:25:32 +0800

    添加DockPanelSuite的测试

---
+++ FormMain.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Media;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WeifenLuo.WinFormsUI.Docking;
+using Timer = System.Windows.Forms.Timer;
+
+namespace WindowsFormsApplication1
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+	    private void Form1_Load(object sender, EventArgs e)
+	    {
+		    this.IsMdiContainer = true;
+	    }
+
+		private void toolStripButton1_Click(object sender, EventArgs e)
+		{
+			FormChild tempForm2 = new FormChild();
+			tempForm2.MdiParent = this;
+			tempForm2.Show(dockPanel1);
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
+            Application.ThreadException += Application_ThreadException;
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FormMain());
+        }
+
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            Exception ex = e.ExceptionObject as Exception;
+            if (ex != null)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        static void Application_ThreadException(object sender, ThreadExceptionEventArgs e)
+        {
+            MessageBox.Show(e.Exception.Message);
+        }
+    }
+}
